c4b32f80f117a45005ce2d95650ffab1e6cc01b7
yuu <yuusei1016@icloud.com>
2024-01-23 16:07:51 +0900

    add installed_app

--- myapp/settings.py
+++ myapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'regex_app.apps.RegexAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
